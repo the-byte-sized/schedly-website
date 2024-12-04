31067cfd5f1ff9d6a5370cc75da592a8bd961ca6
Claudio Cortese <claudio.cortese@>
2024-12-04 16:32:10 +0100

    Improve documentation

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -274,6 +274,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['bash'],
     },
   } as Preset.ThemeConfig,
 };
