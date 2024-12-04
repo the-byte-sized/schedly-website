61cc4ecb2015c54821a1f6343eed69344f3fbbdf
Claudio Cortese <claudio.cortese@>
2024-12-04 18:25:34 +0100

    Enable theme switch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -102,7 +102,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
-      disableSwitch: true,
+      defaultMode: 'light',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
     },
     navbar: {
       title: 'Schedly',
